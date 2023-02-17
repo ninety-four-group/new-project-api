0685c1221d1a39b8c73e5cdb73ce6112e8f0e246
justsaimain <saimain.primary@gmail.com>
2023-02-17 14:48:10 +0630

    category media

--- database/migrations/2023_01_25_035618_create_brands_table.php
+++ database/migrations/2023_01_25_035618_create_brands_table.php
@@ -16,7 +16,7 @@ return new class () extends Migration {
             $table->id();
             $table->string('name');
             $table->string('slug');
-            $table->string('image')->nullable();
+            $table->unsignedBigInteger('image')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
