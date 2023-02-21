e8c30505ac8ea451300dd4229ba279c00f5b5cde
saimain-primary <saimain.primary@gmail.com>
2023-02-21 20:25:34 +0630

    warehouse filter

--- app/Http/Resources/SKUResource.php
+++ app/Http/Resources/SKUResource.php
@@ -14,7 +14,6 @@ class SKUResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
 
         return [
             'id' => $this->id,
