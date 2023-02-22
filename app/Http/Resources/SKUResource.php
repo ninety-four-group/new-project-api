13ba0d173b041c39a332c94da88d3b4b00330c7a
justsaimain <saimain.primary@gmail.com>
2023-02-22 11:56:45 +0630

    sku product

--- app/Http/Resources/SKUResource.php
+++ app/Http/Resources/SKUResource.php
@@ -21,6 +21,7 @@ class SKUResource extends JsonResource
             'variation' => $this->variation,
             'quantity' => $this->quantity,
             'price' => $this->price,
+            'product' => $this->product,
             'status' => $this->status,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
