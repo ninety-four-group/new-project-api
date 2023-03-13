c2d0913f0df9c6080b6a522dd77f48cb47121f4f
saimain-primary <saimain.primary@gmail.com>
2023-03-13 18:13:47 +0630

    variation type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
+        // \App\Models\Admin::factory(1)->create();
+        Admin::create([
+            "name" => "Admin",
+            "email" => "admin@gmail.com",
+            "password" => Hash::make('password'),
+            "email_verified_at" => now()
+        ]);
         // \App\Models\Brand::factory(20)->create();
         // \App\Models\Category::factory(20)->create();
 
